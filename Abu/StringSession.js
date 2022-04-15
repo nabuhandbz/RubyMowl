220e1e55b8306a2aa79b1e8cdc3ae36c7d5031ed
nabuhan-dbz <93925348+nabuhandbz@users.noreply.github.com>
2022-04-15 13:57:03 +0530

    Update StringSession.js

--- Abu/StringSession.js
+++ Abu/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ABU_SESSION' in process.env && string === undefined) {
+        if ('RubyMowl_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'Abu:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Ruby:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
